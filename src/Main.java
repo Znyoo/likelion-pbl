d8a2a7e4baf1bc5b6b5a8169c7a27d29c6c03ce6
박진효 <wlsgy6157@gmail.com>
2026-04-10 20:12:33 +0900

    출력 문구 수정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, LikeLion!");
-    }
+        System.out.println("Hello, LikeLion! - 2nd Commit");    }
 }
